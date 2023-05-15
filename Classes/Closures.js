f49796ea0148e24ad03a69798ee65db5eb0de3c2
tewarimohit <tewarimohit955@gmail.com>
2023-05-15 21:42:23 +0530

    practice

--- Classes/Closures.js
+++ Classes/Closures.js
@@ -54,20 +54,61 @@
 // // const spaceship = liftOff();
 // console.log(liftOff());
 
-function outer() {
-  let a = 40;
-  function inner() {
-    console.log(a);
-  }
+// async function getData() {
+//   const resp = await fetch("https://global-warming.org/api/temperature-api");
+//   const data = await resp.json();
+//   console.log(data);
+// }
 
-  a = 100;
-  return inner;
-}
+// getData();
+// async function getData() {
+//   const resp = await fetch("https://global-warming.org/api/co2-api");
+//   // It is expressed as a mole fraction in dry air, parts per million (ppm).
+//   const data = await resp.json();
+//   console.log(data);
+// }
 
-const returned = outer();
-console.log(returned());
+// getData();
+// async function getData() {
+//   const resp = await fetch(
+//     "https://api.nasa.gov/planetary/apod?api_key=9TgWjlwbGT8oAeGrYwPv63veahX7Mkhf3k1btOBX"
+//   );
+//   // It is expressed as a mole fraction in dry air, parts per million (ppm).
+//   const data = await resp.json();
+//   console.log(data);
+// }
 
-// fgdfsgdfgdfg
+// getData();
+// async function getData() {
+//   const resp = await fetch(
+//     "https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=9TgWjlwbGT8oAeGrYwPv63veahX7Mkhf3k1btOBX"
+//   );
+//   // It is expressed as a mole fraction in dry air, parts per million (ppm).
+//   const data = await resp.json();
+//   console.log(data);
+// }
 
-const miku = outer();
-console.log(miku(), "here");
+// getData();
+
+// const counter = () => {
+//   let count = 1;
+//   const innerCounter = () => {
+//     count = count * 3;
+//     console.log("count", count);
+//   };
+//   return innerCounter;
+// };
+
+// const firstTime = counter();
+// console.log(firstTime());
+// console.log(firstTime());
+// const secondTime = counter();
+// console.log(secondTime());
+
+const getOutput = (title, anyFunction) => {
+  anyFunction(title);
+};
+
+getOutput("Any Title", (a) => {
+  console.log(a);
+});
