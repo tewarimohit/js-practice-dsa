b77be06a75092696f7bae42050a5d958300b4ffd
tewarimohit <tewarimohit955@gmail.com>
2023-07-18 15:34:25 +0530

    codewars question practice

--- Questions/codewars.js
+++ Questions/codewars.js
@@ -114,3 +114,63 @@ function solution(str) {
   }
   return newArr;
 }
+
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+function alphabetPosition(text) {
+  text = text.toLowerCase();
+  let newText = "";
+  const alphaCode = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+
+  for (let character of text) {
+    if (alphaCode[character] !== undefined)
+      newText = newText + " " + alphaCode[character];
+  }
+  text = newText.trim();
+  return text;
+}
+// or
+
+function alphabetPosition(text) {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((t) => letters.indexOf(t) > -1)
+    .map((t) => letters.indexOf(t) + 1 || "")
+    .join(" ");
+}
