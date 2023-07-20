959f8182ce3effd8fee6ca4e3ad5d54a5eb04d26
tewarimohit <tewarimohit955@gmail.com>
2023-07-20 17:29:07 +0530

    are they same codewars

--- Questions/codewars.js
+++ Questions/codewars.js
@@ -204,3 +204,19 @@ console.log(findOdd(array));
 function findOdd(arr) {
   return arr.find((item, index) => arr.filter((el) => el == item).length % 2);
 }
+
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the
+// "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears).
+//  "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.every((item, index) => item * item == array2[index]);
+}
