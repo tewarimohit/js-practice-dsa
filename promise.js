315ab6b31908c8590dc0823be141cdd8afa754ba
tewarimohit <tewarimohit955@gmail.com>
2024-12-19 10:55:44 +0530

    promise add

--- promise.js
+++ promise.js
@@ -19,44 +19,44 @@
 //       console.log("Dhokha hua h :(");
 //     });
 
-const fakeRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const pages = {
-        "/user": [
-          { id: 1, username: "Bilbo" },
-          { id: 2, username: "Laura" },
-        ],
-        "/about": "Here is about page",
-      };
-      const status = "200 OK";
-      const data = pages[url];
-      if (data) {
-        resolve({ status, data });
-      } else {
-        reject({ status: "500 Server Fucked up!!" });
-      }
-    }, 3000);
-  });
-};
+// const fakeRequest = (url) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const pages = {
+//         "/user": [
+//           { id: 1, username: "Bilbo" },
+//           { id: 2, username: "Laura" },
+//         ],
+//         "/about": "Here is about page",
+//       };
+//       const status = "200 OK";
+//       const data = pages[url];
+//       if (data) {
+//         resolve({ status, data });
+//       } else {
+//         reject({ status: "500 Server Fucked up!!" });
+//       }
+//     }, 3000);
+//   });
+// };
 
-fakeRequest("/dogs")
-  .then((res) => {
-    console.log(res.status);
-    console.log(res.data);
-  })
-  .catch((res) => {
-    console.log(res.status);
-  });
+// fakeRequest("/dogs")
+//   .then((res) => {
+//     console.log(res.status);
+//     console.log(res.data);
+//   })
+//   .catch((res) => {
+//     console.log(res.status);
+//   });
 
-fakeRequest("/user")
-  .then((res) => {
-    console.log(res.status);
-    console.log(res.data);
-  })
-  .catch((res) => {
-    console.log(res.status);
-  });
+// fakeRequest("/user")
+//   .then((res) => {
+//     console.log(res.status);
+//     console.log(res.data);
+//   })
+//   .catch((res) => {
+//     console.log(res.status);
+//   });
 
 //   nesting .then syntax
 
@@ -65,3 +65,26 @@ fakeRequest("/user")
 // console.log(res);
 // return fakeRequest(`/user/${id}`)
 //   }).then().catch(works for all)
+
+// const add = (a) => {
+//   return (b) => {
+//     if (b) {
+//       return add(a + b);
+//     } else return a;
+//   };
+// };
+
+// const addition = add(2)(3)(8)();
+// console.log(addition);
+
+const sum = (a) => {
+  return (b) => {
+    if (b) {
+      return sum(a + b);
+    } else {
+      return a;
+    }
+  };
+};
+
+console.log(sum(4)(8)(), "Hereee");
